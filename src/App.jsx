28c4dc593068f174eccd5ed9c5b1ee96a293a6ba
mahadi609im <maha609im@gmail.com>
2025-09-22 15:38:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h3 className="text-center font-semibold text-center text-green-500">
+        Bismillah
+      </h3>
+    </div>
+  );
+};
+
+export default App;
