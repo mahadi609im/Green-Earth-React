f53d2dbfad3e546cf560feb7eb3969483386c6bb
mahadi609im <maha609im@gmail.com>
2025-09-23 07:02:05 +0600

    half complete

--- src/Components/Trees/Trees.jsx
+++ src/Components/Trees/Trees.jsx
@@ -1,23 +1,14 @@
-import React, { Suspense, use } from 'react';
+import React, { use } from 'react';
 import Tree from './Tree';
-import TreeCatagories from '../TreeCatagory/TreeCatagories';
 
 const Trees = ({ plantsData }) => {
   let useTrees = use(plantsData);
-
   let allPlants = useTrees.plants;
-
-  let plantsCatagory = fetch(
-    'https://openapi.programming-hero.com/api/categories'
-  ).then(res => res.json());
+  console.log(allPlants);
 
   return (
     <div className="grid grid-cols-12 gap-[30px]">
-      <div className="col-span-2 ">
-        <Suspense fallback={<h3>Loading...</h3>}>
-          <TreeCatagories plantsCatagory={plantsCatagory}></TreeCatagories>
-        </Suspense>
-      </div>
+      <div className="col-span-2 "></div>
       <div className="col-span-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {allPlants.map(tree => (
           <Tree key={tree.id} tree={tree}></Tree>
